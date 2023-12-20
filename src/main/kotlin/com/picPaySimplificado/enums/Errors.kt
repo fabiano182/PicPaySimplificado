b0dd66da0d986b1e5b706b6bbfdcb058c1ce4c31
oiachuva5 <oiachuva5@gmail.com>
2023-12-20 16:00:14 -0300

    Refatorando codigo

--- src/main/kotlin/com/picPaySimplificado/enums/Errors.kt
+++ src/main/kotlin/com/picPaySimplificado/enums/Errors.kt
@@ -3,12 +3,15 @@ package com.picPaySimplificado.enums
 enum class Errors(val code: String, val message: String) {
     //Generic
     OP001("OP-001", "Invalid Request"),
-    OP002("OP-002", "Request cannot be null"),
+    OP002("OP-002", "Transação negada!"),
     //Customer
     CO001("CO-001", "Customer [%s] não existe."),
     CO002("CO-002", "Não foi possivel atualizar o Customer [%s]."),
     CO003("CO-003", "Não foi possivel deletar o customer [%s]."),
     CO004("CO-004", "Insira um CPF ou um CNPJ"),
     //Transaction
-    TO001("TO-001", "Customer [%s] não existe ou não é lojista.")
+    TO001("TO-001", "Customer [%s] não existe"),
+    TO002("TO-002", "Customer [%s] não é cliente."),
+    TO003("TO-003", "Customer [%s] não é lojista"),
+    TO004("TO-004", "Saldo insuficiente para efetuar esta operação")
 }
--- src/main/kotlin/com/picPaySimplificado/service/CustomerService.kt
+++ src/main/kotlin/com/picPaySimplificado/service/CustomerService.kt
@@ -4,6 +4,7 @@ package com.picPaySimplificado.service
 import com.picPaySimplificado.enums.Errors
 import com.picPaySimplificado.exception.BadRequestException
 import com.picPaySimplificado.model.CustomerModel
+import com.picPaySimplificado.model.TransactionModel
 import com.picPaySimplificado.repository.CustomerRepository
 import jakarta.transaction.Transactional
 import org.springframework.stereotype.Service
@@ -21,18 +22,15 @@ class CustomerService(private val repository: CustomerRepository) {
             11 -> repository.save(customer)
             14 -> repository.save(customer)
             else -> throw BadRequestException(
-                Errors.CO004.message,
-                Errors.CO004.code
+                Errors.CO004.message, Errors.CO004.code
             )
         }
     }
 
     fun findCustomer(id: Int): CustomerModel {
-        return repository.findById(id)
-            .orElseThrow {
+        return repository.findById(id).orElseThrow {
                 com.picPaySimplificado.exception.NotFoundException(
-                    Errors.CO001.message.format(id),
-                    Errors.CO001.code
+                    Errors.CO001.message.format(id), Errors.CO001.code
                 )
             }
     }
@@ -41,8 +39,7 @@ class CustomerService(private val repository: CustomerRepository) {
     fun update(customer: CustomerModel) {
         if (!repository.existsById(customer.id!!)) {
             throw BadRequestException(
-                Errors.CO002.message.format(customer.id),
-                Errors.CO002.code
+                Errors.CO002.message.format(customer.id), Errors.CO002.code
             )
         }
         repository.save(customer)
@@ -51,8 +48,7 @@ class CustomerService(private val repository: CustomerRepository) {
     fun delete(id: Int) {
         if (!repository.existsById(id)) {
             throw BadRequestException(
-                Errors.CO003.message.format(id),
-                Errors.CO003.code
+                Errors.CO003.message.format(id), Errors.CO003.code
             )
         }
         repository.deleteById(id)
@@ -65,5 +61,46 @@ class CustomerService(private val repository: CustomerRepository) {
 
     fun registroGovernoAvailable(registroGoverno: String): Boolean {
         return !repository.existsByRegistroGoverno(registroGoverno)
+    }
+
+    //TRANSFERENCE
+
+    fun senderValidate(transaction: TransactionModel): CustomerModel {
+
+        val senderCheck: Boolean = repository.existsById(transaction.envia)
+        val sender = repository.findById(transaction.envia)
+
+        if (!senderCheck) return throw BadRequestException(
+            Errors.TO001.message.format(transaction.envia), Errors.TO001.code
+        )
+
+        if (!sender.get().ePF()) return throw BadRequestException(
+            Errors.TO002.message.format(transaction.envia), Errors.TO002.code
+        )
+
+        return sender.get()
+    }
+
+    fun merchantValidate(transaction: TransactionModel): CustomerModel {
+
+        val merchantCheck: Boolean = repository.existsById(transaction.recebe)
+        val merchant = repository.findById(transaction.recebe)
+
+        if (!merchantCheck) throw BadRequestException(
+            Errors.TO001.message.format(transaction.envia), Errors.TO001.code
+        )
+
+        if (!merchant.get().ePJ()) throw BadRequestException(
+            Errors.TO003.message.format(transaction.envia), Errors.TO003.code
+        )
+
+        return merchant.get()
+    }
+
+    fun checkBalance(valor: Float, senderBalance: Float): Boolean {
+        if (valor <= senderBalance) return true
+        return throw BadRequestException(
+            Errors.TO004.message, Errors.TO004.code
+        )
     }
 }
