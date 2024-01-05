e210736a5b14dbf77df8c9e8f5695bb767550665
oiachuva5 <oiachuva5@gmail.com>
2024-01-04 22:04:59 -0300

    Criado teste TransactionService (getAll e getTransaction), Criado mais testes para CustomerService (create, create Exception,  emailAvailable e emailAvailable exception)

--- src/main/kotlin/com/picPaySimplificado/enums/Errors.kt
+++ src/main/kotlin/com/picPaySimplificado/enums/Errors.kt
@@ -10,7 +10,7 @@ enum class Errors(val code: String, val message: String) {
     CO003("CO-003", "Não foi possivel deletar o customer [%s]."),
     CO004("CO-004", "Insira um CPF ou um CNPJ"),
     //Transaction
-    TO001("TO-001", "Customer [%s] não existe"),
+    TO001("TO-001", "Transação [%s] não existe"),
     TO002("TO-002", "Customer [%s] não é cliente."),
     TO003("TO-003", "Customer [%s] não é lojista"),
     TO004("TO-004", "Saldo insuficiente para efetuar esta operação")
